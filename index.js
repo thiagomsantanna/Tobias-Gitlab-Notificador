430cab595e737200b53ce10b7c54f78fae2c8a4a
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-01-03 08:31:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const expss = require('express');
+const app = expss();
+const port = 3000;
+
+app.get('/jooj', (req, res) => {
+    res.sendStatus(200);
+})
+
+app.get('/', (req, res) => {
+    //res.send('fodase');
+    res.send(`${req.body}`);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
