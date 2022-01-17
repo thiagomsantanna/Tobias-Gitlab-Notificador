3a2459f0fb4169c09e496046507eda32a5ec9c8d
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-01-17 14:58:34 -0300

    asdfewqwqe

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/jooj', (req, res) => {
 })
 
 app.post('/testgitlob', (req, res) => {
-    res.stauts(200).send(req.body);
+    res.status(200).send(req.body);
     // console.log(req);
     // res.send('-------------------');
     // res.send(req.body);
