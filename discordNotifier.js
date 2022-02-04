abba48e6b9f49bd076075243867d85db1887b2ef
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-02-04 09:03:40 -0300

    sapao oficial

--- discordNotifier.js
+++ discordNotifier.js
@@ -14,7 +14,7 @@ function buildEmbedMessage(issue) {
               name: "Homologation"
             },
             thumbnail: {
-              url: "https://github.com/thiagomsantanna/apiGitLabNotifier/blob/5ceb342a0679fec4cab396b62e55d41f80fda406/assets/sapo.gif"
+              url: "https://raw.githubusercontent.com/thiagomsantanna/gitlab-issues-notifications/master/imgs/sapo.gif"
             }
         }]
     };
